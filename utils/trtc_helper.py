3bcda8226859cab2f30b2aeaef26cab9409da545
z-xin-puls <2086298420@qq.com>
2026-04-20 23:20:16 +0800

    Initial commit

--- utils/trtc_helper.py
+++ utils/trtc_helper.py
@@ -1,5 +1,3 @@
-import TLSSigAPIv2
-
 # TRTC 配置
 TRTC_SDKAPPID = 1600138234
 TRTC_SECRET_KEY = "db11b7d48e1afd90b0b10bfd9cada42cbf851f43f178ea3204d63deea6044d32"
@@ -10,7 +8,6 @@ def gen_user_sig(user_id, expire=86400):
     if not user_id:
         user_id = "guest"
 
-    # 使用腾讯云官方库生成UserSig
-    api = TLSSigAPIv2.TLSSigAPIv2(TRTC_SDKAPPID, TRTC_SECRET_KEY)
-    user_sig = api.gen_sig(user_id, expire)
-    return user_sig
+    # TODO: 需要下载TLSSigAPIv2.py源码到本地或使用其他方案
+    # 目前前端使用手动生成的UserSig进行测试
+    raise NotImplementedError("请使用手动生成的UserSig或下载TLSSigAPIv2.py源码")

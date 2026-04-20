3402bbcae7e0d738bd8a1cc1e3c6fb3e9a519cb6
z-xin-puls <2086298420@qq.com>
2026-04-21 00:00:04 +0800

    Initial commit

--- utils/trtc_helper.py
+++ utils/trtc_helper.py
@@ -1,4 +1,4 @@
-from TLSSigAPIv2 import TLSSigAPIv2
+from .TLSSigAPIv2 import TLSSigAPIv2
 
 TRTC_SDKAPPID = 1600138234
 TRTC_SECRET_KEY = "db11b7d48e1afd90b0b10bfd9cada42cbf851f43f178ea3204d63deea6044d32"
@@ -9,6 +9,6 @@ def gen_user_sig(user_id, expire=604800):
         user_id = "guest"
 
     # 使用腾讯云官方库生成UserSig
-    api = TLSSigAPIv2.TLSSigAPIv2(TRTC_SDKAPPID, TRTC_SECRET_KEY)
+    api = TLSSigAPIv2(TRTC_SDKAPPID, TRTC_SECRET_KEY)
     user_sig = api.genUserSig(user_id, expire)
     return user_sig
